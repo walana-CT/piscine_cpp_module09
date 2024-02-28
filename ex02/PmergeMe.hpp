969befe33cbbf0fba6b89a024279a7f2ceeb6cf7
walana <robin.ficht@free.fr>
2024-02-28 11:36:24 +0100

    yhaoooooooooo j'ai fini

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: rficht <rficht@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:38:18 by rficht            #+#    #+#             */
-/*   Updated: 2024/02/27 18:20:15 by rficht           ###   ########.fr       */
+/*   Updated: 2024/02/28 11:33:11 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,42 +17,66 @@
 #include <cstdlib>
 #include <iostream>
 #include <algorithm>
-#define N 40
-
+#include <time.h>
+#define N 20
 
 template <typename T>
-T insertSort(T c)
+void printCont(const T input, int color)
 {
-    int i, key, j;
-    for (i = 1; i < n; i++) 
-	{
-        key = arr[i];
-        j = i - 1;
-        while (j >= 0 && arr[j] > key) 
-		{
-            arr[j + 1] = arr[j];
-            j = j - 1;
-        }
-        arr[j + 1] = key;
-    }
-	
+	int size = input.size();
+
+	if (color == 0)
+		for (int i = 0; i < size && i < 10; i++)
+			std::cout << input[i] << " ";
+	else
+		for (int i = 0; i < size && i < 10; i++)
+			std::cout << "\u001b[48;5;" << i/2+color << "m " << input[i] << " ";
+	if (size > 10)
+		std::cout << "...";
+	std::cout << "\u001b[0m" << std::endl;	
 }
 
 template <typename T>
-T mergeSort(T &c)
+void insertSort(T &c)
+{
+	unsigned int x, j, i =1;
+
+	while (i < c.size())
+	{
+		x = c[i];
+		j = i;
+		while (j > 0 && c[j-1] > x)
+		{
+			c[j] = c[j-1];
+			j--;		
+		}		
+		c[j] = x;
+		i++;		
+	}	
+}
+
+template <typename T>
+void mergeSort(T &c)
 {
 	if (c.size() <= N)
 		insertSort(c);
 	else
 	{
-		T c1 = T(c.begin(), c.end()/2);
-		T c2 = T(c.end()/2, c.end());
+
+		typename T::iterator itMidle = c.begin();
+		for (size_t i = 0; i < c.size()/2; i++)
+			itMidle++;
+		
+		T c1 = T(c.begin(), itMidle);
+		T c2 = T(itMidle, c.end());
 
 		mergeSort(c1);
 		mergeSort(c2);
 		
 		c.clear();
 
-		std::merge(c1.begin(), c1.end(), c2.begin(), c2.end(), c.begin());
+		std::merge(c1.begin(), c1.end(), c2.begin(), c2.end(), std::back_inserter(c));
 	}
 }
+
+bool isStrDigit(const char* str);
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,55 +6,56 @@
 /*   By: rficht <rficht@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 16:36:08 by rficht            #+#    #+#             */
-/*   Updated: 2024/02/27 17:53:30 by rficht           ###   ########.fr       */
+/*   Updated: 2024/02/28 11:35:08 by rficht           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include"PmergeMe.hpp"
 
 
-bool isStrDigit(const char* str)
-{
-	while (*str)
-		if (!isdigit(*str++))
-			return false;
-	return true;
-}
-
-void printInput(const std::vector<unsigned int>& input)
-{
-	int size = input.size();
-	std::cout << "Before: ";	
-	for (int i = 0; i < size && i < 10; i++)
-		std::cout << input[i] << " ";
-	if (size > 10)
-		std::cout << "...";
-	std::cout << std::endl;	
-}
-
-
 int main(int argc, char const *argv[])
 {
-	std::vector<unsigned int> vect, input;
-	std::deque<unsigned int> deq;
+	std::vector<unsigned int> input;
+	clock_t time;
+	int mult = 2;
 
 	if (argc == 1)
 		return 0;
+
+	std::cout << std::endl;
 
 	for (int i = 1; i < argc; i++)
 	{
 		if (!isStrDigit(argv[1]))
 			return(std::cout << "please enter postive int only" << std::endl, 1);
-		input.push_back(std::atoi(argv[i]));
+		for (int j = 0; j < mult; j++)
+			input.push_back(std::atoi(argv[i]) + j);
 	}
 	
-	printInput(input);
-	
-	std::vector<unsigned int> vect(input);
-	std::deque<unsigned int> deq(input.begin(), input.end());
+	std::cout << "\u001b[1m \u001b[4m INPUT: \u001b[0m" << std::endl;		
+	printCont(input, 0);
+	std::cout << "smallest group elem:" << N << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "\u001b[1m \u001b[4m Vector: \u001b[0m" << std::endl;	
 
-	
-	
+	//VECTOR
+	std::vector<unsigned int> vect(input);
+	time = clock();
+	mergeSort(vect);
+	time = clock() - time;
+	std::cout << "computation time: " << "\u001b[38;5;" << 202 << "m " << ((float)time/CLOCKS_PER_SEC) << "seconds \u001b[0m" << std::endl;
+	printCont(vect, 202);
+	std::cout << std::endl << std::endl;
+
+	//DEQUE
+	std::cout << "\u001b[1m \u001b[4m Deque: \u001b[0m" << std::endl;
+	std::deque<unsigned int> deq(input.begin(), input.end());	
+	time = clock();	
+	mergeSort(deq);
+	time = clock() - time;
+	std::cout << "computation time: " << "\u001b[38;5;" << 160 << "m " << ((float)time/CLOCKS_PER_SEC) << "seconds \u001b[0m" << std::endl;	
+	printCont(deq, 88);
+	std::cout << std::endl;
 
 	return 0;
 }
